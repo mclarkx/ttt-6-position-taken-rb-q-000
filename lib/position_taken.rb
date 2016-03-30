00e775b6e74a71e0637016d89af17c9a6b5e4682
mclarkx <micahclarkx@gmail.com>
2016-03-30 14:36:56 +0000

    nitrous-backup at Wed Mar 30 14:36:56 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] || ["", " ", " ", " ", " ", " ", " ", " ", " "] || [nil, " ", " ", " ", " ", " ", " ", " ", " "]
+  position == [0]
+  end
+  if board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+  end
+end
